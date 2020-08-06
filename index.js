1708e977d768e89dcaa7a1a389ae5cbc708e001e
Tom Monks <monksy@gmail.com>
2020-08-06 14:19:54 -0400

    Moved change formatting to index.js
    - improved reusability of Service Desk module
    - servicedesk.js/getChanges() now returns changes in raw Service Desk format
    - index.js/formatChanges() formats changes into Smartsheet row format

--- index.js
+++ index.js
@@ -3,28 +3,69 @@ const smartsheetId = "6991861635147652";
 const serviceDesk = require("./servicedesk");
 const smartSheet = require("./smartsheet");
 
+// convert changes from Service Desk to rows for Smartsheet
+const formatChanges = (changes) => {
+  return changes.map((change) => {
+    return {
+      toBottom: true,
+      cells: [
+        {
+          // id
+          value: change.id,
+          columnId: 2712127197734788,
+        },
+        {
+          // Title
+          value: change.name,
+          columnId: 1586227290892164,
+        },
+        {
+          // Department
+          value: change.department ? change.department.name : "",
+          columnId: 3838027104577412,
+        },
+        {
+          // State
+          value: change.state,
+          columnId: 5526876964841348,
+        },
+        {
+          // Assignee Name
+          value: change.assignee ? change.assignee.name : "",
+          columnId: 2149177244313476,
+        },
+        {
+          // Priority
+          value: change.priority,
+          columnId: 8904576685369220,
+        },
+      ],
+    };
+  });
+};
+
 // Main function
 const updateSmartsheet = async (smartsheetId) => {
   try {
     let rowsToDelete = await smartSheet.getRowIds(smartsheetId);
     let deletionResult = await smartSheet.deleteRowsById(smartsheetId, rowsToDelete);
-    console.log(deletionResult.message + ": deleted " + deletionResult.result.length + " rows");
-    const changes = await serviceDesk.getChanges();
-    console.log(`Inserting ${changes.length} changes...`);
-    let addResult = await smartSheet.addRows(smartsheetId, changes);
-    console.log(addResult.message + ": added " + addResult.result.length + " rows");
+    // console.log(deletionResult.message + ": deleted " + deletionResult.result.length + " rows");
+    const serviceDeskChanges = await serviceDesk.getChanges();
+    const smartSheetRows = await formatChanges(serviceDeskChanges);
+    // console.log(`Inserting ${smartSheetRows.length} changes...`);
+    let addResult = await smartSheet.addRows(smartsheetId, smartSheetRows);
+    // console.log(addResult.message + ": added " + addResult.result.length + " rows");
   } catch (err) {
     console.error(err);
   }
 };
 
-// addRowsToSmartsheet(smartsheetId);
 updateSmartsheet(smartsheetId);
 
 /* Testing */
 // (async () => {
 //   try {
-//     console.log(await smartSheet.addRows(1234, {}));
+//     console.log(await reformatChanges(await serviceDesk.getChanges()));
 //   } catch (err) {
 //     console.error("Something went wrong!");
 //     console.error(err.message);
--- servicedesk.js
+++ servicedesk.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 require("dotenv").config();
 
-// Retrieve all current changes from ServiceDesk
+// Get all current changes from ServiceDesk
 module.exports.getChanges = async () => {
   const BASE_URL = "https://api.samanage.com";
   const HEADERS = {
@@ -27,51 +27,11 @@ module.exports.getChanges = async () => {
 
     try {
       const results = await axios.get(BASE_URL + "/changes.json", requestOptions);
-      // console.log(`found ${results.data.length} changes`);
 
       totalPages = results.headers["x-total-pages"] || 1;
-      // console.log("Page " + currentPage + ": Found " + results.data.length + " incidents");
       currentPage++;
 
-      changes = changes.concat(
-        results.data.map((change) => {
-          return {
-            toBottom: true,
-            cells: [
-              {
-                // id
-                value: change.id,
-                columnId: 2712127197734788,
-              },
-              {
-                // Title
-                value: change.name,
-                columnId: 1586227290892164,
-              },
-              {
-                // Department
-                value: change.department ? change.department.name : "",
-                columnId: 3838027104577412,
-              },
-              {
-                // State
-                value: change.state,
-                columnId: 5526876964841348,
-              },
-              {
-                // Assignee Name
-                value: change.assignee ? change.assignee.name : "",
-                columnId: 2149177244313476,
-              },
-              {
-                // Priority
-                value: change.priority,
-                columnId: 8904576685369220,
-              },
-            ],
-          };
-        })
-      );
+      changes = changes.concat(results.data);
     } catch (err) {
       err.message = "Unable to get changes from Service Desk: " + err.message;
       throw err;
